9244bcc1e0b4fea9ea01d5ce5943b39ed7c70124
iama3191 <ire.andreina.z@gmail.com>
2022-03-28 17:26:22 +0000

    add validation of integer values

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def get_sales_data():
     print('Example: 10, 20, 30, 40, 50, 60\n')
 
     data_str = input('Enter your data here: ')
-    
+      
     sales_data = data_str.split(',')
     validate_data(sales_data)
 
@@ -33,7 +33,9 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there are not exactly values.
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f'Exactly 6 values required, you provided {len(values)}'
