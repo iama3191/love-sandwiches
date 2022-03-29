8af9f43af3e7d005c76f9bf1f39f94d5b8b2403b
iama3191 <ire.andreina.z@gmail.com>
2022-03-29 08:06:58 +0000

    update surplus worksheet

--- run.py
+++ run.py
@@ -88,6 +88,16 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def update_surplus_worksheet(surplus_data):
+    """
+    Update superplus worksheet, add new row with the list data provided
+    """
+    print('Updating surplus worksheet...\n')
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(surplus_data)
+    print('Sales worksheet updated successfully. \n')
+
+
 def main():
     """
     Run all program functions
@@ -96,6 +106,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
     print(new_surplus_data)
 
 
